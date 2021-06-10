1fcadf6b01147ea2d2ed32cfd9b919476ad29be1
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-10 14:58:44 +0200

    exercice 4.3 fait

---
+++ exercice4.3.php
@@ -0,0 +1,22 @@
+<?php
+        $string1 = 'Voici une première chaîne de caractères';
+        $string2 = 'Voici une deuxième chaîne de caractères';
+        function stringFunction($string1, $string2) : string 
+        {
+        return stringFunction($string1, $string2);
+}?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 4.3</title>
+</head>
+<body>
+    <h1>Exercice 3 Partie 4 : Les fonctions PHP</h1>
+    <p><?= $string1?></p>
+    <p><?=$string2?></p>
+    
+</body>
+</html>
